24b6bf541e5a72b931c7aeb836e60953341b9fc9
Sam Harwell <sam@tunnelvisionlabs.com>
2017-01-24 07:14:07 -0600

    Update assembly info for release 4.1.0

--- Tvl.VisualStudio.MouseNavigation/Properties/AssemblyInfo.cs
+++ Tvl.VisualStudio.MouseNavigation/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.1.0.0")]
 [assembly: AssemblyFileVersion("4.1.0.0")]
-[assembly: AssemblyInformationalVersion("4.1.0-dev")]
+[assembly: AssemblyInformationalVersion("4.1.0")]
